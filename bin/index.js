5129631f5e6f7dafa9fcd6bb9ec943a7e667a2fd
leohai <448456376@qq.com>
2021-05-20 22:42:28 +0800

    feat:简单的脚手架实现原理

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
 console.log("my-cli")
+const { argv } = require('process')
+const [command, options] = argv.slice(2)
+if (command === 'init') {
+  if (options) {
+    console.log(`app init with ${options}`)
+  } else {
+    console.log(`app init by on params`)
+  }
+}
